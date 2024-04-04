b22293d621084057e14df70ce9a1755808c9fab7
brent_anthuenis <brent.anthuenis@outlook.com>
2024-04-04 16:03:33 +0200

    kleine_fix_supabase_repo

--- Jogging.Infrastructure/Repositories/SupabaseRepos/AuthenticationRepo.cs
+++ Jogging.Infrastructure/Repositories/SupabaseRepos/AuthenticationRepo.cs
@@ -1,19 +1,24 @@
 ﻿using Jogging.Infrastructure.Interfaces;
 using Jogging.Infrastructure.Models;
+using Supabase;
 
 namespace Jogging.Infrastructure.Repositories.SupabaseRepos
 {
     internal class AuthenticationRepo : IAuthenticationRepo
     {
-        Supabase.Client _client;
+        Client _client;
 
-        public PersonRepo(Client client)
+
+        public AuthenticationRepo(Client client)
         {
             _client = client;
         }
         public Person Authenticate(string email, string psswd)
         {
-            throw new NotImplementedException();
+            var session = _client.Auth.SignIn(email, psswd);
+            var userid = session.Result.User.Id;
+            var person = _client.From<Person>().Where(x => x.UserId == userid).Single().Result;
+            return person;
         }
     }
 }
